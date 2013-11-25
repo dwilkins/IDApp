3e224b1fed84cf11e7e5fb6ec17d12f9ec279111
David H. Wilkins <dwilkins@conecuh.com>
2013-11-25 11:12:41 -0600

    For Openshift:
     * config.assets.initialize_on_precompile = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ IDApp::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.assets.initialize_on_precompile = false
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
